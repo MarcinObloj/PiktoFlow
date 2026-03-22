76abfcef77d04263114379744adf467d1dd14f0f
Marcin <Marcix444@gmail.com>
2026-03-22 18:01:46 +0100

    feat(lang): add Polish translations for validation and auth

---
+++ lang/pl/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Błędny adres e-mail lub hasło.',
+    'password' => 'Podane hasło jest nieprawidłowe.',
+    'throttle' => 'Za dużo prób logowania. Spróbuj ponownie za :seconds sekund.',
+];
---
+++ lang/pl/validation.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'confirmed' => 'Hasła nie są takie same.',
+    'email' => 'Podaj poprawny :attribute.',
+    'max' => [
+        'string' => 'Pole :attribute nie może mieć więcej niż :max znaków.',
+    ],
+    'min' => [
+        'string' => 'Pole :attribute musi mieć co najmniej :min znaków.',
+    ],
+    'required' => 'Pole :attribute jest wymagane.',
+    'unique' => 'Ten :attribute jest już zajęty w systemie.',
+
+
+    'attributes' => [
+        'name' => 'imię/nazwa',
+        'email' => 'adres e-mail',
+        'password' => 'hasło',
+    ],
+];
